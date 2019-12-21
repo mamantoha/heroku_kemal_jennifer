02a7bc7e5525258afccbc3b83cd7518965f9ee9b
Anton Maminov <anton.linux@gmail.com>
2019-12-21 16:59:41 +0200

    format code

--- db/migrations/20191221134532959_create_users.cr
+++ db/migrations/20191221134532959_create_users.cr
@@ -1,8 +1,8 @@
 class CreateUsers < Jennifer::Migration::Base
   def up
     create_table :users do |t|
-      t.string :name, { :null => false }
-      t.integer :age, { :null => false }
+      t.string :name, {:null => false}
+      t.integer :age, {:null => false}
 
       t.timestamps
     end
